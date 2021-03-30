71e9930b9e3938f24b3a1b01b0b5d0413e228269
Dhika Rizky <deka_r@outlook.com>
2021-03-30 20:28:20 +0700

    Added current user detail and logout function

--- testsite/urls.py
+++ testsite/urls.py
@@ -21,6 +21,7 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
